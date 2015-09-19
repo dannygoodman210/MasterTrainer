b7adcda23f9949f791544150b0aa9beb0d29bb6d
Danny Goodman <dannygoodman210@gmail.com>
2015-09-19 14:37:38 -0400

    Started on Deck Class

--- src/cards/Deck.java
+++ src/cards/Deck.java
@@ -1,5 +1,21 @@
 package cards;
 
-public class Deck {
+import java.io.File;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Stack;
+
+public class Deck{
+	
+	private List<Object> myDeck;
+	
+	public Deck(File f){
+		//Parse Text File to create Deck.
+	}
+	
+	public void shuffle(){
+		Collections.shuffle(myDeck);
+	}
 
 }
